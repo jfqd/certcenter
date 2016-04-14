e8db6cf53d00154b597a11d171dc56eeed6853e4
jfqd <jfqd@blun.org>
2016-04-14 11:47:56 +0200

    skip SAN extension if domain_list is empty

--- lib/certcenter/pki.rb
+++ lib/certcenter/pki.rb
@@ -14,7 +14,7 @@ module Certcenter
   # )
   class Pki
     
-    KEYSIZE = 4096 # or only 2048 ?
+    KEYSIZE = 4096
     
     # Generate X509 CSR (certificate signing request) with SAN (Subject Alternative Name) extension and sign it with the RSA key
     def self.generate_csr(common_name, organization, country, state_name, locality, domain_list)
@@ -36,19 +36,20 @@ module Certcenter
       csr.subject = subject
       csr.public_key = signing_key.public_key
       
-      # prepare SAN extension
-      san_list = domain_list.map { |domain| "DNS:#{domain}" }
-      extensions = [
-        OpenSSL::X509::ExtensionFactory.new.create_extension('subjectAltName', san_list.join(','))
-      ]
-      
-      # add SAN extension to the CSR
-      attribute_values = OpenSSL::ASN1::Set [OpenSSL::ASN1::Sequence(extensions)]
-      [
-        OpenSSL::X509::Attribute.new('extReq', attribute_values),
-        OpenSSL::X509::Attribute.new('msExtReq', attribute_values)
-      ].each do |attribute|
-        csr.add_attribute attribute
+      if domain_list != nil && domain_list != []
+        # prepare SAN extension
+        san_list = domain_list.map { |domain| "DNS:#{domain}" }
+        extensions = [
+          OpenSSL::X509::ExtensionFactory.new.create_extension('subjectAltName', san_list.join(','))
+        ]
+        # add SAN extension to the CSR
+        attribute_values = OpenSSL::ASN1::Set [OpenSSL::ASN1::Sequence(extensions)]
+        [
+          OpenSSL::X509::Attribute.new('extReq', attribute_values),
+          OpenSSL::X509::Attribute.new('msExtReq', attribute_values)
+        ].each do |attribute|
+          csr.add_attribute attribute
+        end
       end
       
       # sign CSR with the signing key
