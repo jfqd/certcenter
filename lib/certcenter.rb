1cebe92e176eaeea93dbea4566debccdfa4076b7
jfqd <jfqd@blun.org>
2016-12-06 20:03:12 +0100

    add todo hint

--- lib/certcenter.rb
+++ lib/certcenter.rb
@@ -9,6 +9,7 @@ module Certcenter
   
   # Wrapper Method
   def self.new(common_name, organization, country, state_name, locality, domain_list=[], oauth_token="", product_code=Certcenter::ALWAYSONSSL)
+    # todo: parameter validation
     ::Certcenter::Api.new(common_name, organization, country, state_name, locality, domain_list, oauth_token, product_code)
   end
   
